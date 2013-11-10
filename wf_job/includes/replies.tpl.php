f64ee56212de27b64bf644a8a7fef52e76eb5889
jribeiro <jribeiro@1975716.no-reply.drupal.org>
2013-11-10 21:21:39 +1100

    Issue #2099783 by jribeiro, dgallinari, skwashd: Switch from reply to wf_log & streamline start job form

--- wf_job/includes/replies.tpl.php
+++
@@ -1,6 +0,0 @@
-<div class="replies-wrapper">
-  <?php if (!empty($header)) : ?><div class="replies-header"><h3><?php print $header ?></h3></div><?php endif; ?>
-  <ul class="replies"><?php print render($replies) ?></ul>
-  <?php if ($links): ?><div class="replies-links"><?php print render($links) ?></div><?php endif ?>
-  <?php if ($reply_form): ?><div class="replies-form"><?php print render($reply_form) ?></div><?php endif; ?>
-</div>
