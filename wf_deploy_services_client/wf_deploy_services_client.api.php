f78eb35fb070a131abdb1ea40968b1885be76571
Dave Hall <dave.hall@skwashd.com>
2015-07-18 02:19:15 +1000

    Add alter hook

--- wf_deploy_services_client/wf_deploy_services_client.api.php
+++ wf_deploy_services_client/wf_deploy_services_client.api.php
@@ -22,5 +22,19 @@ function hook_wf_deploy_services_client_content_diff_data_alter(&$diff, $job) {
 }
 
 /**
+ * Alter the WF deploy services client config.
+ *
+ * @param array $config
+ *   The client config to be altered.
+ * @param WfJob $job
+ *   The job the client relates to.
+ */
+function hook_wf_deploy_services_client_remote_config_alter(&$config, $job) {
+  $handler = wf_job_get_visit_plugin_handler();
+  $url = parse_url($config['url']);
+  $url['scheme'] = 'https';
+  $config['url'] = $handler->buildUrl($url);
+}
+/**
  * @} End of "addtogroup hooks".
  */
